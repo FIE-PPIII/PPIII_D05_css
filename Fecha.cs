73fb3df30886aa19a60b5ee1af1bd58a4016ff21
Juan Bautista Nemi <80529710+jnemi2@users.noreply.github.com>
2022-04-04 19:38:09 -0300

    ADD - D05 css

---
+++ Fecha.cs
@@ -0,0 +1,167 @@
+﻿using System;
+
+public class Fecha
+{
+    private int dd;
+    private int mm;
+    private int aa;
+
+    public Fecha()
+    { }
+
+    public Fecha(int dd, int mm, int aa)
+    {
+        this.dd = dd;
+        this.mm = mm;
+        this.aa = aa;
+    }
+
+    public int DD
+    {
+        get { return this.dd; }
+        set { this.dd = value; }
+    }
+
+    public int MM
+    {
+        get { return this.mm; }
+        set { this.mm = value; }
+    }
+
+    public int AA
+    {
+        get { return this.aa; }
+        set { this.aa = value; }
+    }
+
+    private int ProcesarCompararFecha(int dd, int mm, int aa)
+    {
+        if (this.aa < aa)
+        {
+            return (-1);
+        }
+
+        if (this.aa > aa)
+        {
+            return (1);
+        }
+
+        if (this.mm < mm)
+        {
+            return (-1);
+        }
+
+        if (this.mm > mm)
+        {
+            return (1);
+        }
+
+        if (this.dd < dd)
+        {
+            return (-1);
+        }
+
+        if (this.dd > dd)
+        {
+            return (1);
+        }
+
+        return (0);
+    }
+
+    public int CompararFecha(int dd, int mm, int aa)
+    {
+        return (this.ProcesarCompararFecha(dd, mm, aa));
+    }
+
+    public int CompararFecha(Fecha fecha)
+    {
+        return(this.ProcesarCompararFecha(fecha.dd, fecha.mm, fecha.aa));
+    }
+
+    public int ValidarFecha()
+    {
+        if (this.aa < 0)
+        {
+            return (-3);
+        }
+
+        if ((this.mm < 1) || (this.mm > 12))
+        {
+            return (-2);
+        }
+
+        if (this.dd < 1)
+        {
+            return (-1);
+        }
+
+        switch (this.mm)
+        {
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 8:
+            case 10:
+            case 12:
+                if (this.dd > 31)
+                {
+                    return (-1);
+                }
+
+                break;
+
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                if (this.dd > 30)
+                {
+                    return (-1);
+                }
+
+                break;
+            case 2:
+                if ((this.aa % 4) != 0)
+                {
+                    if (this.dd > 28)
+                    {
+                        return (-1);
+                    }
+
+                    break;
+                }
+
+                if ((this.aa % 100) == 0)
+                {
+                    if ((this.aa % 400) != 0)
+                    {
+                        if (this.dd > 28)
+                        {
+                            return (-1);
+                        }
+                    }
+                }
+
+                if (this.dd > 29)
+                {
+                    return (-1);
+                }
+
+                break;
+
+            default:
+                break;
+        }
+
+        return (0);
+    }
+
+    public void MostrarFecha()
+    {
+        Console.WriteLine("Fecha: {0}/{1}/{2}", this.dd, this.mm, this.aa);
+
+        return;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+
+namespace Diseño_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int estado;
+            Fecha[] vectorFecha = new Fecha[3];
+
+            vectorFecha[0] = new Fecha(12, 4, 2019);
+
+            vectorFecha[1] = new Fecha(7, 5, 2019);
+
+            vectorFecha[2] = new Fecha(23, 6, 2019);
+
+            foreach (Fecha fecha in vectorFecha)
+            {
+                Console.WriteLine("\nDatos de la Fecha");
+
+                fecha.MostrarFecha();
+
+                estado = fecha.ValidarFecha();
+
+                if (estado == -1)
+                {
+                    Console.WriteLine("El dia es incorrecto {0}", fecha.DD);
+                }
+
+                if (estado == -2)
+                {
+                    Console.WriteLine("El mes es incorrecto {0}", fecha.MM);
+                }
+
+                if (estado == -3)
+                {
+                    Console.WriteLine("El a#o es menor a cero {0}", fecha.AA);
+                }
+
+                if (estado != 0)
+                {
+                    Console.ReadKey();
+
+                    return;
+                }
+            }
+
+            Console.WriteLine("\nCompara los tres atributos de la primera fecha y los tres atributos de segunda fecha enviados como parametros");
+            estado = vectorFecha[0].CompararFecha(vectorFecha[1].DD, vectorFecha[1].MM, vectorFecha[1].AA);
+
+            if (estado == 1)
+            {
+                Console.WriteLine("La primera {0}/{1}/{2} es mayor a la segunda {3}/{4}/{5}", vectorFecha[0].DD, vectorFecha[0].MM, vectorFecha[0].AA, vectorFecha[1].DD, vectorFecha[1].MM, vectorFecha[1].AA);
+            }
+            else
+            {
+                if (estado == -1)
+                {
+                    Console.WriteLine("La segunda {0}/{1}/{2} es mayor a la primera {3}/{4}/{5}", vectorFecha[1].DD, vectorFecha[1].MM, vectorFecha[1].AA, vectorFecha[0].DD, vectorFecha[0].MM, vectorFecha[0].AA);
+                }
+                else
+                {
+                    Console.WriteLine("La primera {0}/{1}/{2} es igual a la segunda {3}/{4}/{5}", vectorFecha[0].DD, vectorFecha[0].MM, vectorFecha[0].AA, vectorFecha[1].DD, vectorFecha[1].MM, vectorFecha[1].AA);
+                }
+            }
+
+            Console.WriteLine("\nCompara los tres atributos de la primera fecha y envio la tercera fecha");
+            estado = vectorFecha[0].CompararFecha(vectorFecha[2]);
+
+            if (estado == 1)
+            {
+                Console.WriteLine("La primera {0}/{1}/{2} es mayor a la tercera {3}/{4}/{5}", vectorFecha[0].DD, vectorFecha[0].MM, vectorFecha[0].AA, vectorFecha[2].DD, vectorFecha[2].MM, vectorFecha[2].AA);
+            }
+            else
+            {
+                if (estado == -1)
+                {
+                    Console.WriteLine("La tercera {0}/{1}/{2} es mayor a la primera {3}/{4}/{5}", vectorFecha[2].DD, vectorFecha[2].MM, vectorFecha[2].AA, vectorFecha[0].DD, vectorFecha[0].MM, vectorFecha[0].AA);
+                }
+                else
+                {
+                    Console.WriteLine("La primera {0}/{1}/{2} es igual a la tercera {3}/{4}/{5}", vectorFecha[0].DD, vectorFecha[0].MM, vectorFecha[0].AA, vectorFecha[2].DD, vectorFecha[2].MM, vectorFecha[2].AA);
+                }
+            }
+
+            Console.WriteLine("\nCompara los tres atributos de la segunda fecha y envio la tercera fecha");
+            estado = vectorFecha[1].CompararFecha(vectorFecha[2]);
+
+            if (estado == 1)
+            {
+                Console.WriteLine("La segunda {0}/{1}/{2} es mayor a la tercera {3}/{4}/{5}", vectorFecha[1].DD, vectorFecha[1].MM, vectorFecha[1].AA, vectorFecha[2].DD, vectorFecha[2].MM, vectorFecha[2].AA);
+            }
+            else
+            {
+                if (estado == -1)
+                {
+                    Console.WriteLine("La tercera {0}/{1}/{2} es mayor a la segunda {3}/{4}/{5}", vectorFecha[2].DD, vectorFecha[2].MM, vectorFecha[2].AA, vectorFecha[1].DD, vectorFecha[1].MM, vectorFecha[1].AA);
+                }
+                else
+                {
+                    Console.WriteLine("La segunda {0}/{1}/{2} es igual a la tercera {3}/{4}/{5}", vectorFecha[1].DD, vectorFecha[1].MM, vectorFecha[1].AA, vectorFecha[2].DD, vectorFecha[2].MM, vectorFecha[2].AA);
+                }
+            }
+
+            Console.ReadKey();
+
+            return;
+        }
+    }
+}
